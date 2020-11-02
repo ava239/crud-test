40c7a3a247c958d5dc1717f1164457fdcfcefea0
ava <ava239@yandex.ru>
2020-11-02 21:22:57 +0300

    Add Https for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Paginator::defaultView('pagination::bootstrap-4');
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
